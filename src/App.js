7c8327480759d9f7e548d42c15d3cd0d4b69f042
Evance23 <evancebarracks01@gmail.com>
2023-06-29 23:54:49 +0300

    add nav files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 const App = ( ) => {
   return (
     <div className="App">
       <h1> Kibetech </h1>
-      <p>Home of Solutions</p>
+     <Navbar/>
     </div>
   );
 }
