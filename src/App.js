8bc369bd95074dfa6d3e18f2b04e4d525a018d0f
Evance23 <evancebarracks01@gmail.com>
2023-07-06 02:57:02 +0300

    style hero section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Hero from "./components/Hero.js";
 
-import './App.css';
-import Navbar from './components/Navbar.js';
-import Hero from './components/Hero.js';
-
-const App = ( ) => {
+const App = () => {
   return (
     <div className="App">
       <h1> Kibetech </h1>
-     <Navbar/>
-     <Hero
-     cName='hero'
-     heroImg='https://wallpapersafari.com/4k-tech-wallpaper/'
-     title='Tech Solutions'
-     text='We address you problems through definite solutions'
-     buttonText='Learn More'
-     url='/'
-     btnClass='show'
-     
-     
-     />
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg="https://pngtree.com/freebackground/electronic-technology-website-texture-background-banner_1065222.html"
+        title="Tech Solutions"
+        text="We address you problems through definite solutions"
+        buttonText="Learn More"
+        url="/"
+        btnClass="show"
+      />
     </div>
   );
-}
+};
 
 export default App;
