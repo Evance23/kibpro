c8213e4666675e36d34ee7593df8ccf68c765629
Evance23 <evancebarracks01@gmail.com>
2023-07-07 04:29:50 +0300

    Adding social media icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,44 @@
 import "./FooterStyles.css";
 
 import React from "react";
-import { FaHome, FaPhone, FaMailBulk } from "react-icons/fa";
+import {
+  FaHome,
+  FaPhone,
+  FaMailBulk,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="right"></div>
+        <div className="right">
+          <h4>Kibetech.</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#088F8F", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#088F8F", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#088F8F", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
         <div className="left">
           <div className="location">
             <FaHome
