51f65c377b4067b137cd201ae579645f4bfb2e9a
Evance23 <evancebarracks01@gmail.com>
2023-07-07 03:48:30 +0300

    Add email-footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,46 @@
-import "./FooterStyles.css"
+import "./FooterStyles.css";
 
-
-import React from 'react'
-import {FaHome, FaPhone} from 'react-icons/fa';
+import React from "react";
+import { FaHome, FaPhone, FaMailBulk } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer-container">
-            <div className="right"></div>
-            <div className="left">
-                <div className="location">
-                    <FaHome size={20} style={{color:'#088F8F', marginRight:"2rem"}}/>
-                    <div>
-                        <p>Kibetech</p>
-                        <p>Nairobi</p>
-
-                    </div>
-                    <div className="phone">
-                        <h4><FaPhone size={20} style={{color:'#088F8F', marginRight:"2rem"}}/>0701020304</h4>
-                    
-                    </div>
-                </div>
+      <div className="footer-container">
+        <div className="right"></div>
+        <div className="left">
+          <div className="location">
+            <FaHome
+              size={20}
+              style={{ color: "#088F8F", marginRight: "2rem" }}
+            />
+            <div>
+              <p>Kibetech</p>
+              <p>Nairobi</p>
             </div>
+            <div className="phone">
+              <h4>
+                <FaPhone
+                  size={20}
+                  style={{ color: "#088F8F", marginRight: "2rem" }}
+                />
+                0701020304
+              </h4>
+            </div>
+            <div className="email">
+              <h4>
+                <FaMailBulk
+                  size={20}
+                  style={{ color: "#088F8F", marginRight: "2rem" }}
+                />
+                evancesoftdev@gmail.com
+              </h4>
+            </div>
+          </div>
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
