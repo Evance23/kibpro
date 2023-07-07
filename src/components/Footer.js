f5a7366f12ced20c10cfb321404935d4ee99ff7a
Evance23 <evancebarracks01@gmail.com>
2023-07-07 03:44:48 +0300

    Adding footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import "./FooterStyles.css"
 
 
 import React from 'react'
+import {FaHome, FaPhone} from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -10,7 +11,16 @@ const Footer = () => {
             <div className="right"></div>
             <div className="left">
                 <div className="location">
-                    <FaHome/>
+                    <FaHome size={20} style={{color:'#088F8F', marginRight:"2rem"}}/>
+                    <div>
+                        <p>Kibetech</p>
+                        <p>Nairobi</p>
+
+                    </div>
+                    <div className="phone">
+                        <h4><FaPhone size={20} style={{color:'#088F8F', marginRight:"2rem"}}/>0701020304</h4>
+                    
+                    </div>
                 </div>
             </div>
         </div>
