6f522edf95138c4d8f9051e80a40fcd53bcdb642
Evance23 <evancebarracks01@gmail.com>
2023-07-14 07:12:14 +0300

    Add changes

--- src/routes/Contacts.js
+++ src/routes/Contacts.js
@@ -1,11 +1,31 @@
-import React from 'react'
+// import React from "react";
+// // import { Form } from "semantic-ui-react";
 
-const Contacts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+// const Contacts = () => {
+//   return (
+//     // <div className="contactform">
+//     //   <Form>
+//     //     <label>
+//     //       First Name
+//     //       <input input type="text" />
+//     //     </label>
+//     //     <label>
+//     //      Second Name
+//     //       <input input type="text" />
+//     //     </label>
+//     //     <label>
+//     //       Email
+//     //       <input input type="text" />
+//     //     </label>
+//     //     <label>
+//     //       Messsage
+//     //       <input input type="text" />
+//     //     </label>
+//     //     <button> Send 
+//     //       </button>
+//     //   </Form>
+//     // </div>
+//   // );
+// // };
 
-export default Contacts
+// export default Contacts;
