5093a9d6d4eaff7e23d6f438f12bec20158235a1
Evance23 <evancebarracks01@gmail.com>
2023-07-14 05:51:38 +0300

    changing the hero section pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,32 @@
 import "./HeroStyles.css";
 
-// function Hero(props) {
-//   return (
-//     <>
-//       <div className={props.cName}>
-//         <img alt="HeroImg" src={props.heroImg} />
-//         <div className="hero-text">
-//           <h1>{props.title}</h1>
-//           <p>{props.text}</p>
-//           <a href={props.url} className={props.btnClass}>
-//             {props.buttonText}
-//           </a>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
 
-// export default Hero;
+import React from "react";
+import IntroImg from "../assets/210888.jpg"
+// import { Link } from "react-router-dom";
+
+
+
 const Hero = () => {
-  return <div>Hero</div>
+  return <div className="hero">
+    <div className="mask">
+      <img className="into-img" src={IntroImg} alt="IntroImg"/>
+      </div>
+      <div className="content">
+        <p style={{color:'whitesmoke'}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem.
+        </p>
+        <h1 style={{color:'whitesmoke'}}>
+Kibetech Solutions 
+        </h1>
+<div>
+  
+  <button className="button">Learn More</button>
+</div>
+      </div>
+      </div>
 };
 export default Hero;
