43cdf0ba472ff53c4cc60bba97d9db0b5271e643
Evance23 <evancebarracks01@gmail.com>
2023-07-18 03:05:44 +0300

    Add the solutions pages

--- src/routes/Solutions.js
+++ src/routes/Solutions.js
@@ -3,8 +3,17 @@ import React from 'react'
 const Solutions = () => {
   return (
     <div>
-      
+      <h1>Our Varied Solutions</h1>
+      <div>
+      <div>Education</div>
+      <div>Education</div>
+      <div>Education</div>
+      <div>Education</div>
+      <div>Education</div>
+      <div>Education</div>
+      <div>Education</div>
     </div>
+    </div> 
   )
 }
 
