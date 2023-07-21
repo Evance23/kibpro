b2279957cb07abc207d8cb0624d2ea79f11f2d86
Evance23 <evancebarracks01@gmail.com>
2023-07-21 04:33:48 +0300

    adding a workcard

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,11 +1,20 @@
 import "./WorkCardStyles.css"
 
 
-import React from 'react'
+import React from 'react';
+import pro1 from "../assets/clasrm.jpg"
+
 
 const WorkCard = () => {
   return (
-    <div>
+    <div className="work-container">
+        <h1 className="solutions-heading">Solutions</h1>
+        <div className="project-container">
+            <div className="project-card">
+                <img alt="img" src={pro1}/>
+
+            </div>
+        </div>
       
     </div>
   )
